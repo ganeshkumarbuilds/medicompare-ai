0ce7710579d18810dc1f5b38792bee29002249b8
ganeshkumarbuilds <ganeshkumar.chavala@gmail.com>
2026-09-05 13:17:41 +0530

    fixed the bugs

--- server/src/main/java/com/medicompare/user/controller/UserAuthController.java
+++ server/src/main/java/com/medicompare/user/controller/UserAuthController.java
@@ -14,10 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/auth")
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-})
 public class UserAuthController {
 
     private final UserService userService;
